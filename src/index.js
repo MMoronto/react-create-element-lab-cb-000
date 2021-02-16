73412f59e7197982a92b91d049603ef56ae8d188
MOBOLA MORONTO <t2wycemo@gmail.com>
2021-02-16 04:02:43 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 const heading = React.createElement('h1', {}, "An Awesome Person")
 const paragraph = React.createElement('p', {}, "Who is learning React")
 const list = React.createElement('ul', {className: 'my-interests'}, [
+  React.createElement('li', {}, 'JavaScript'),
   ])
 
 ReactDOM.render(
