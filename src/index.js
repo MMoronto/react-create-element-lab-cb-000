f575b76a2e9899f74f8f26d96a8fcc68d67ad8f2
MOBOLA MORONTO <t2wycemo@gmail.com>
2021-02-16 04:01:40 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const heading = React.createElement('h1', {}, "An Awesome Person")
-const paragraph = React.createElement('h1', {}, "An Awesome Person")
-const heading = React.createElement('h1', {}, "An Awesome Person")
+const paragraph = React.createElement('p', {}, "Who is learning React")
+const list = React.createElement('ul', {className: 'my-interests'}, [
+  ])
 
 ReactDOM.render(
   meInReact,
