c3d93676284724550767efdfdccf8dfb93030c7c
MOBOLA MORONTO <t2wycemo@gmail.com>
2021-02-16 04:06:25 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -5,7 +5,15 @@ const heading = React.createElement('h1', {}, "An Awesome Person")
 const paragraph = React.createElement('p', {}, "Who is learning React")
 const list = React.createElement('ul', {className: 'my-interests'}, [
   React.createElement('li', {}, 'JavaScript'),
+  React.createElement('li', {}, 'React'),
+  React.createElement('li', {}, 'Movies'),
+  React.createElement('li', {}, 'Ice cream'),
   ])
+  
+  const meInReact = React.createElement('div', {className: 'me'}, [
+    heading, paragraph, list
+    ]
+  )
 
 ReactDOM.render(
   meInReact,
