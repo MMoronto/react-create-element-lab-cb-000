65317a6b671f7fc83559e062d6489d4a1bb4d37a
MOBOLA MORONTO <t2wycemo@gmail.com>
2021-02-16 03:59:41 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const heading = React.createElement('h1', {}, "An Awesome Person")
+const paragraph = React.createElement('h1', {}, "An Awesome Person")
+const heading = React.createElement('h1', {}, "An Awesome Person")
 
 ReactDOM.render(
   meInReact,
